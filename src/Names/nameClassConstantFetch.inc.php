d3d2a34f796ccd480722d8a38f7ac9aeb8574fff
Netmosfera <netmosfera@gmail.com>
2019-02-06 11:46:52 +0100

    Improve nameClassConstantFetch, adds checks for "self", "parent" and "static" other than disallowing void::CLASS

--- src/Names/nameClassConstantFetch.inc.php
+++ src/Names/nameClassConstantFetch.inc.php
@@ -2,13 +2,34 @@
 
 namespace Netmosfera\PHPParserTools\Names;
 
+use Exception;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\Scalar\String_;
 
-function classNameConstFetch($name): Expr{
+/**
+ * Returns the AST for `ProvidedName::CLASS` or equivalent.
+ *
+ * @throws          Exception
+ *
+ * @param           Name|Identifier|NULL $name
+ *
+ * @param           Bool $allowSelf
+ *
+ * @param           Bool $allowParent
+ *
+ * @param           Bool $allowStatic
+ *
+ * @return          Expr
+ */
+function classNameConstFetch(
+    $name,
+    Bool $allowSelf = TRUE,
+    Bool $allowParent = TRUE,
+    Bool $allowStatic = TRUE
+): Expr{
 
     if($name instanceof Identifier){
         $name = new Name($name->name);
@@ -16,6 +37,23 @@ function classNameConstFetch($name): Expr{
 
     if(count($name->parts) === 1){
         $nameString = strtolower($name->parts[0]);
+
+        if($nameString === "self" && !$allowSelf){
+            throw new Exception("`self::CLASS` is disallowed");
+        }
+
+        if($nameString === "parent" && !$allowParent){
+            throw new Exception("`parent::CLASS` is disallowed");
+        }
+
+        if($nameString === "static" && !$allowStatic){
+            throw new Exception("`static::CLASS` is disallowed");
+        }
+
+        if($nameString === "void"){
+            throw new Exception("`void::CLASS` is disallowed");
+        }
+
         if($nameString === "callable" || $nameString === "array"){
             return new String_($nameString);
         }
